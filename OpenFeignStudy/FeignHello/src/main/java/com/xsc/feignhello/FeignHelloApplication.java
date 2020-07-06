68f49ce4182bffdbe8d88fc2177f50fe05f39b57
Jakexsc <649199985@qq.com>
2020-07-06 23:44:28 +0800

    OpenFeign日志打印

--- OpenFeignStudy/FeignHello/src/main/java/com/xsc/feignhello/FeignHelloApplication.java
+++ OpenFeignStudy/FeignHello/src/main/java/com/xsc/feignhello/FeignHelloApplication.java
@@ -1,8 +1,10 @@
 package com.xsc.feignhello;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author JakeXsc
@@ -13,6 +15,16 @@ public class FeignHelloApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FeignHelloApplication.class, args);
+    }
+
+    /**
+     * 开启Feign日志打印
+     *
+     * @return
+     */
+    @Bean
+    Logger.Level loggerLevel() {
+        return Logger.Level.FULL;
     }
 
 }
