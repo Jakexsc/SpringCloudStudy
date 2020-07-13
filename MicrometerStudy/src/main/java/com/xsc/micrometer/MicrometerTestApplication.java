7ef5b850051e45368fa9adaf6c8578ac632e725c
Jakexsc <649199985@qq.com>
2020-07-14 00:32:53 +0800

    Micrometer and Prometheus Test

---
+++ MicrometerStudy/src/main/java/com/xsc/micrometer/MicrometerTestApplication.java
@@ -0,0 +1,16 @@
+package com.xsc.micrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jakexsc
+ */
+@SpringBootApplication
+public class MicrometerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrometerTestApplication.class, args);
+    }
+
+}
---
+++ MicrometerStudy/src/test/java/com/xsc/micrometer/MicrometerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsc.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
