0d3fde451aa42f2a4141719d1ffe7244bb75c895
Jakexsc <649199985@qq.com>
2020-07-14 23:19:11 +0800

    Zuul HelloWorld

---
+++ ZuulStudy/ZuulHello/src/main/java/com/xsc/zuul/ZuulApplication.java
@@ -0,0 +1,17 @@
+package com.xsc.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Jakexsc
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
---
+++ ZuulStudy/ZuulHello/src/test/java/com/xsc/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsc.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
