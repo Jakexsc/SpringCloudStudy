b37304153db7f670fb42583574ace993352151f1
Jakexsc <649199985@qq.com>
2020-07-15 22:51:05 +0800

    ConfigClient

---
+++ ConfigStudy/ConfigClient/src/main/java/com/xsc/configclient/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.xsc.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jakexsc
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
---
+++ ConfigStudy/ConfigClient/src/main/java/com/xsc/configclient/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.xsc.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author JakeXsc
+ * @version 1.0
+ * @date 2020/7/15 22:47
+ */
+@RestController
+public class HelloController {
+    @Value("${xsc}")
+    String xsc;
+    @GetMapping("/hello")
+    public String hello() {
+        return xsc;
+    }
+}
